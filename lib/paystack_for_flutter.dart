d5da23e44f3a02cf0b8a83852c8aee44068001da
Victor <bukkychukwujekwu@gmail.com>
2025-04-19 01:50:53 +0100

    added first name and last name fields to be saved on Paystack dashboard for customer, added support for Euro Currency.

--- lib/paystack_for_flutter.dart
+++ lib/paystack_for_flutter.dart
@@ -1,3 +1,4 @@
 //expose these files for use.
 export 'package:paystack_for_flutter/src/paystack_flutter.dart';
 export 'package:paystack_for_flutter/src/core/utils/functions.dart';
+export 'package:paystack_for_flutter/src/models/customer.dart';
--- lib/src/models/customer.dart
+++ lib/src/models/customer.dart
@@ -2,18 +2,18 @@ class Customer {
   final String? firstName;
   final String? lastName;
   final String email;
-  //final String? phone;
+  final String? phone;
   Customer({
     this.firstName,
     this.lastName,
     required this.email,
-    //this.phone,
+    this.phone,
 });
 
   Map<String, dynamic> toJson() => {
     'first_name': firstName,
     'last_name': lastName,
     'email': email,
-    //'phone_number': phone,
+    'phone_number': phone,
   };
 }
